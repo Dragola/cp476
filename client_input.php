4ac3f36ad8a8ece6c61d8c32f171c22840acce75
Bryan Gadd <x-dragola-x@live.ca>
2023-03-11 21:04:51 -0500

    Added SELECT functionailty
    Changes:
    - Added textbox to input form for entering student names/id's. Use comma to seperate student's.
    - Recieve and process the students names in the reults script (clean-up, split, and detrmine if student was referenced by id or name).

--- client_input.php
+++ client_input.php
@@ -4,12 +4,19 @@
         
         <label for="response">Would you like to SELECT or UPDATE the database? </label>
         <br>
-        <input type="radio" id="response" name="response" value="SELECT" checked="true">
+        <input type="radio" id="mode" name="mode" value="SELECT" checked="true">
         <label for="radio">SELECT</label>
-        <input type="radio" id="response" name="response" value="UPDATE">
+        <input type="radio" id="mode" name="mode" value="UPDATE">
         <label for="radio">UPDATE</label>
+        <br>
+        <br>
+        <br>
+        <label for="students">Id/Name of students to select (use , to seperate)</label>
+        <br>
+        <textarea name="students" rows="2" cols="30"></textarea><br>
         
-        <!-- <br>
+        <!-- 
+        <br>
         <label for="name">Name:</label>
         <select name="formal" id="formal">
           <option value="Mr">Mr</option>
@@ -19,12 +26,9 @@
         <br>
         <label for="email">Email Address:</label>
         <input type="text" id="email" name="email" value="larry@example.com"><br>
-        <br>
-        
-        <br>
-        <label for="comments">Comments:</label>
-        <textarea name="comments" rows="2" cols="30"></textarea><br>
-        <br> -->
-        <input type="submit" value="Send my Feedback"><br>
+        <br> 
+        -->
+      
+        <input type="submit" value="Call database"><br>
       </form> 
 </html>
