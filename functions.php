e6b15c70deaa2d6028a7552c54c29627678142cb
ArticWhite <col.morenz@gmail.com>
2023-03-19 11:45:52 -0400

    update and sql injection protection added

--- functions.php
+++ functions.php
@@ -10,10 +10,15 @@ function grabStudentCoursesName($servername, $username,$password, $DB,$studentna
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
-
-    $sql = "SELECT * FROM Course_Table WHERE Student_ID IN ( SELECT Student_ID FROM Name_Table WHERE Student_Name = '$studentname')";
-    $result = $conn->query($sql);
+    $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table INNER JOIN Name_Table ON Name_Table.Student_ID=Course_Table.Student_ID WHERE Student_Name = ?";
+    
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("s", $studentname);
+    $stmt->execute();
+    $result = $stmt->get_result();
     $conn->close();
+    $stmt->close();
+
     return $result;
 }
 /*
@@ -27,16 +32,21 @@ function grabStudentCoursesID($servername, $username,$password, $DB,$studentid){
     if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
     }
-    
-        $sql = "SELECT * FROM Course_Table WHERE Student_ID = $studentid";
-        $result = $conn->query($sql);
-        $conn->close();
-        return $result;
+        
+    $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table, Name_Table WHERE Name_Table.Student_ID=Course_Table.Student_ID AND Course_Table.Student_ID = ?";
+    $stmt = $conn->prepare($sql);
+    $stmt->bind_param("i", $studentid);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $conn->close();
+    $stmt->close();
+
+    return $result;
 }
 /*
 mass query of the database for every entry.
 */
-function queryAll($servername, $username,$password, $DB){
+function queryAll($servername, $username, $password, $DB){
     // Check connection
     $conn = new mysqli($servername, $username, $password, $DB);
     // Check connection
@@ -44,10 +54,47 @@ function queryAll($servername, $username,$password, $DB){
     die("Connection failed: " . $conn->connect_error);
     }
     
-        $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table INNER JOIN Name_Table ON Name_Table.Student_ID=Course_Table.Student_ID ORDER BY Student_ID";
+        $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table INNER JOIN Name_Table ON Name_Table.Student_ID=Course_Table.Student_ID ORDER BY Student_ID, Course_Code";
         $result = $conn->query($sql);
         $conn->close();
         return $result;
+}
+
+function updateTest($servername, $username, $password, $DB, $studentid,$course,$test1,$test2,$test3,$final){
+    $conn = new mysqli($servername, $username, $password, $DB);
+    // Check connection
+    if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+    }
+    if (is_numeric($test1)){
+        $sql = "UPDATE Course_Table SET Test1 = ? WHERE Student_ID = ? AND Course_Code = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("iis", $test1, $studentid,$course);
+        $stmt->execute();
+        $stmt->close();
+    }
+    if (is_numeric($test2)){
+        $sql = "UPDATE Course_Table SET Test2 = ? WHERE Student_ID=? AND Course_Code = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("iis", $test2, $studentid, $course);
+        $stmt->execute();
+        $stmt->close();
+    }
+    if (is_numeric($test3)){
+        $sql = "UPDATE Course_Table SET Test3 = ? WHERE Student_ID=? AND Course_Code = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("iis", $test3, $studentid,$course);
+        $stmt->execute();
+        $stmt->close();
+    }
+    if (is_numeric($final)){
+        $sql = "UPDATE Course_Table SET Final = ? WHERE Student_ID=? AND Course_Code = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("iis", $final, $studentid,$course);
+        $stmt->execute();
+        $stmt->close();
+    }
+    $conn->close();
 }
 
 ?>
--- testFunction.php
+++ testFunction.php
@@ -6,20 +6,35 @@ $password="mniwpp.12345";
 $db = "StudentsDatabase";
 $nametable = "Name_Table";
 $coursetable = "Course_Table";
-
+/*
+251173274 Xiao Qiang PS275 86.0 64.0 65.0 59.0
+251173274 Xiao Qiang EC140 98.0 58.0 59.0 52.0
+*/
 $studentname = "Xiao Qiang";
 $studentid = 251173274;
+$studentcourse="PS275";
+$test1=86;
+$test2=64;
+$test3=65;
+$final=59;
 
 $table = grabStudentCoursesName($servername, $username,$password, $db,$studentname);
 foreach ( $table as $var ) {
-    echo "\n", $var['Student_ID'], " ", $var['Course_Code']," ",$var['Test1'], " ",$var['Test2'], " ", $var['Test3'], " " , $var['Final'];
+    echo "\n", $var['Student_ID'], " ",$var["Student_Name"] ," ", $var['Course_Code']," ",$var['Test1'], " ",$var['Test2'], " ", $var['Test3'], " " , $var['Final'];
 }
+
+/*
 $table = grabStudentCoursesID($servername, $username,$password, $db,$studentid);
 foreach ( $table as $var ) {
-    echo "\n", $var['Student_ID'], " ", $var['Course_Code']," ",$var['Test1'], " ",$var['Test2'], " ", $var['Test3'], " " , $var['Final'];
+    echo "\n", $var['Student_ID'], " ",$var["Student_Name"] ," ", $var['Course_Code']," ",$var['Test1'], " ",$var['Test2'], " ", $var['Test3'], " " , $var['Final'];
 }
+
+
 $table = queryAll($servername, $username,$password, $db);
 foreach ( $table as $var ) {
     echo "\n", $var['Student_ID'], " ",$var["Student_Name"]," ", $var['Course_Code']," ",$var['Test1'], " ",$var['Test2'], " ", $var['Test3'], " " , $var['Final'];
 }
+*/
+updateTest($servername, $username, $password, $db, $studentid,$studentcourse,$test1,$test2,$test3,$final)
+
 ?>
