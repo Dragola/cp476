0cf88f79cc46662de5b5045ce6ba7c97b3550d82
ArticWhite <col.morenz@gmail.com>
2023-03-22 16:40:47 -0400

    Database function now an object

--- functions.php
+++ functions.php
@@ -3,95 +3,114 @@
 function takes servername, username, password, database, and student
 query'd the database for courses student under students name 
 */
-function grabStudentCoursesName($servername, $username,$password, $DB,$studentname){
-    // Create connection
-    $conn = new mysqli($servername, $username, $password, $DB);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
-    $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table INNER JOIN Name_Table ON Name_Table.Student_ID=Course_Table.Student_ID WHERE Student_Name = ?";
-    
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $studentname);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $conn->close();
-    $stmt->close();
-
-    return $result;
-}
-/*
-function takes servername, username, password, database, and student id
-query'd the database for courses student under students id
-*/
-function grabStudentCoursesID($servername, $username,$password, $DB,$studentid){
-    // Check connection
-    $conn = new mysqli($servername, $username, $password, $DB);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    }
+class Database{
+    private $servername = "localhost";
+    private $username;
+    private $password;
+    private $DB = "StudentsDatabase";
+    function __construct($username,$password)
+    {
+        $this->username = $username;
+        $this->password = $password;
         
-    $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table, Name_Table WHERE Name_Table.Student_ID=Course_Table.Student_ID AND Course_Table.Student_ID = ?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("i", $studentid);
-    $stmt->execute();
-    $result = $stmt->get_result();
-    $conn->close();
-    $stmt->close();
-
-    return $result;
-}
-/*
-mass query of the database for every entry.
-*/
-function queryAll($servername, $username, $password, $DB){
-    // Check connection
-    $conn = new mysqli($servername, $username, $password, $DB);
-    // Check connection
-    if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
     }
-    
-        $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table INNER JOIN Name_Table ON Name_Table.Student_ID=Course_Table.Student_ID ORDER BY Student_ID, Course_Code";
-        $result = $conn->query($sql);
-        $conn->close();
-        return $result;
-}
+    function __destruct()
+    {
+        echo 'database connection closed';
+    }
 
-function updateTest($servername, $username, $password, $DB, $studentid, $course, $test, $grade){
-    $conn = new mysqli($servername, $username, $password, $DB);
-    // Check connection
-    if ($conn->connect_error) {
+    function grabStudentCoursesName($studentname){
+        // Create connection
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->DB);
+        // Check connection
+        if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
+        }
+        $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table INNER JOIN Name_Table ON Name_Table.Student_ID=Course_Table.Student_ID WHERE Student_Name = ?";
+        
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $studentname);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        $stmt->close();
+
+        return $result;
     }
-    switch ($test) {
-        case 1:
-            $sql = "UPDATE Course_Table SET Test1 = ? WHERE Student_ID = ? AND Course_Code = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("iis", $grade, $studentid,$course);
-            $stmt->execute();
-            break;
-        case 2:
-            $sql = "UPDATE Course_Table SET Test2 = ? WHERE Student_ID = ? AND Course_Code = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("iis", $grade, $studentid, $course);
-            $stmt->execute();
-            break;
-        case 3:
-            $sql = "UPDATE Course_Table SET Test3 = ? WHERE Student_ID = ? AND Course_Code = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("iis", $grade, $studentid,$course);
-            $stmt->execute();
-            break;
-        case 4:
-            $sql = "UPDATE Course_Table SET Final = ? WHERE Student_ID = ? AND Course_Code = ?";
-            $stmt = $conn->prepare($sql);
-            $stmt->bind_param("iis", $grade, $studentid,$course);
-            $stmt->execute();
-            break;
+    /*
+    function takes servername, username, password, database, and student id
+    query'd the database for courses student under students id
+    */
+    function grabStudentCoursesID($studentid){
+        // Check connection
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->DB);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+            
+        $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table, Name_Table WHERE Name_Table.Student_ID=Course_Table.Student_ID AND Course_Table.Student_ID = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("i", $studentid);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $conn->close();
+        $stmt->close();
+
+        return $result;
     }
-    $conn->close();
+    /*
+    mass query of the database for every entry.
+    */
+    function queryAll(){
+        // Check connection
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->DB);
+        // Check connection
+        if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+        }
+        
+            $sql = "SELECT Course_Table.Student_ID, Name_Table.Student_Name,Course_Table.Course_Code, Course_Table.Test1, Course_Table.Test2, Course_Table.Test3, Course_Table.Final FROM Course_Table INNER JOIN Name_Table ON Name_Table.Student_ID=Course_Table.Student_ID ORDER BY Student_ID, Course_Code";
+            $result = $conn->query($sql);
+            $conn->close();
+            return $result;
+    }
+    /*
+    updates student's test within a clourse, updates either test1 test2 test3 or final
+    */
+    function updateTest($studentid, $course, $test, $grade){
+        $conn = new mysqli($this->servername, $this->username, $this->password, $this->DB);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        switch ($test) {
+            case 1:
+                $sql = "UPDATE Course_Table SET Test1 = ? WHERE Student_ID = ? AND Course_Code = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("iis", $grade, $studentid,$course);
+                $stmt->execute();
+                break;
+            case 2:
+                $sql = "UPDATE Course_Table SET Test2 = ? WHERE Student_ID = ? AND Course_Code = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("iis", $grade, $studentid, $course);
+                $stmt->execute();
+                break;
+            case 3:
+                $sql = "UPDATE Course_Table SET Test3 = ? WHERE Student_ID = ? AND Course_Code = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("iis", $grade, $studentid,$course);
+                $stmt->execute();
+                break;
+            case 4:
+                $sql = "UPDATE Course_Table SET Final = ? WHERE Student_ID = ? AND Course_Code = ?";
+                $stmt = $conn->prepare($sql);
+                $stmt->bind_param("iis", $grade, $studentid,$course);
+                $stmt->execute();
+                break;
+        }
+        $conn->close();
+    }
 }
 ?>
--- testFunction.php
+++ testFunction.php
@@ -1,11 +1,13 @@
 <?php
 require __DIR__ . '/functions.php';
+/*
 $servername = "localhost";
+*/
 $username = "root";
 $password="mniwpp.12345";
+/*
 $db = "StudentsDatabase";
-$nametable = "Name_Table";
-$coursetable = "Course_Table";
+*/
 /*
 251173274 Xiao Qiang PS275 86.0 64.0 65.0 59.0
 251173274 Xiao Qiang EC140 98.0 58.0 59.0 52.0
@@ -13,28 +15,28 @@ $coursetable = "Course_Table";
 $studentname = "Xiao Qiang";
 $studentid = 251173274;
 $studentcourse="PS275";
-$test1=86;
-$test2=64;
-$test3=65;
-$final=59;
+$test=1;
+$grade=50;
 
-$table = grabStudentCoursesName($servername, $username,$password, $db,$studentname);
+$database = new Database($username,$password);
+
+$table = $database->grabStudentCoursesName($studentname);
 foreach ( $table as $var ) {
     echo "\n", $var['Student_ID'], " ",$var["Student_Name"] ," ", $var['Course_Code']," ",$var['Test1'], " ",$var['Test2'], " ", $var['Test3'], " " , $var['Final'];
 }
 
 /*
-$table = grabStudentCoursesID($servername, $username,$password, $db,$studentid);
+$table = grabStudentCoursesID($studentid);
 foreach ( $table as $var ) {
     echo "\n", $var['Student_ID'], " ",$var["Student_Name"] ," ", $var['Course_Code']," ",$var['Test1'], " ",$var['Test2'], " ", $var['Test3'], " " , $var['Final'];
 }
 
 
-$table = queryAll($servername, $username,$password, $db);
+$table = queryAll();
 foreach ( $table as $var ) {
     echo "\n", $var['Student_ID'], " ",$var["Student_Name"]," ", $var['Course_Code']," ",$var['Test1'], " ",$var['Test2'], " ", $var['Test3'], " " , $var['Final'];
 }
 */
-updateTest($servername, $username, $password, $db, $studentid,$studentcourse,$test1,$test2,$test3,$final)
+//updateTest($studentid,$studentcourse,$test,$grade)
 
 ?>
